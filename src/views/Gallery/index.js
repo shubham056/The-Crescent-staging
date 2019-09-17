a097aa4850726da37ae92992e65af68415ea732e
shubham <shubham.k@cwsinfotech.com>
2019-09-17 18:27:10 +0530

    gallery changes

--- src/views/Gallery/index.js
+++ src/views/Gallery/index.js
@@ -1,16 +1,31 @@
 import React from "react";
-
+import ModalImage from "react-modal-image";
 import Box from "../../components/Box";
 import { Image, ImageWrap } from "./styles";
+import { CenterFocusStrong } from "styled-icons/material";
 
 function Gallery({ images, ...rest }) {
+ 
+
+
   return (
     <Box display="flex" flexWrap="wrap" {...rest}>
       {images.map((src, index) => (
         <ImageWrap key={index} width={[1, 1 / 3, 1 / 4]}>
-          <Image backgroundImage={`url(${src})`} />
+          {/* <Image 
+          backgroundImage={`url(${src})`} 
+           /> */}
+            <ModalImage
+              className="ReactModelImage"
+              small={src}
+              large={src}
+              hideDownload={true}
+              hideZoom={true}
+             />
+    
         </ImageWrap>
       ))}
+    
     </Box>
   );
 }
