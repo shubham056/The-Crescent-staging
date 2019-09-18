4f1208cabb1039cd3ea55c70704ebafa297efc63
shubham <shubham.k@cwsinfotech.com>
2019-09-18 19:26:52 +0530

    news

--- gatsby-config.js
+++ gatsby-config.js
@@ -72,7 +72,7 @@ module.exports = {
       options: {
         apiURL: `http://157.245.132.14:1337`,
         queryLimit: 1000, // Default to 100
-        contentTypes: [`galleries`,`dinewithuses`,`retalandhospitalities`,`news2017S`,`news`],
+        contentTypes: [`galleries`,`dinewithuses`,`retalandhospitalities`,,`news`],
         // Possibility to login with a strapi user, when content types are not publically available (optional).
         loginData: {
           identifier: "",
